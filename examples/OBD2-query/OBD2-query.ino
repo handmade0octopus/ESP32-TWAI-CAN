8ef363384ea4a02868ba484913f7897c477b62db
mdxs <van.wijgerden@mdxs.net>
2025-03-07 23:06:07 +0100

    Fixed example typo (in name/path), description, and code layout

--- examples/OBD2-query/OBD2-query.ino
+++ examples/OBD2-query/OBD2-query.ino
@@ -1,7 +1,7 @@
-#include <ESP32-TWAI-CAN.hpp>
+// Example sketch for the ESP32-TWAI-CAN library driver,
+// showing how to query OBD2 over TWAI / CAN for coolant temperature.
 
-// Simple sketch that querries OBD2 over CAN for coolant temperature
-// Showcasing simple use of ESP32-TWAI-CAN library driver.
+#include <ESP32-TWAI-CAN.hpp>
 
 // Default for ESP32
 #define CAN_TX 5
@@ -17,9 +17,9 @@ void sendObdFrame(uint8_t obdId) {
     obdFrame.data[0]          = 2;
     obdFrame.data[1]          = 1;
     obdFrame.data[2]          = obdId;
-    obdFrame.data[3]          = 0xAA; // Best to use 0xAA (0b10101010) instead of 0
-    obdFrame.data[4]          = 0xAA; // CAN works better this way as it needs
-    obdFrame.data[5]          = 0xAA; // to avoid bit-stuffing
+    obdFrame.data[3]          = 0xAA; // Best use 0xAA (0b10101010) instead of 0
+    obdFrame.data[4]          = 0xAA; // TWAI / CAN works better this way, as it
+    obdFrame.data[5]          = 0xAA; // needs to avoid bit-stuffing
     obdFrame.data[6]          = 0xAA;
     obdFrame.data[7]          = 0xAA;
     // Accepts both pointers and references
@@ -42,7 +42,7 @@ void setup() {
     ESP32Can.setSpeed(ESP32Can.convertSpeed(500));
 
     // You can also just use .begin()..
-    if (ESP32Can.begin()) {
+    if(ESP32Can.begin()) {
         Serial.println("CAN bus started!");
     } else {
         Serial.println("CAN bus failed!");
@@ -50,7 +50,7 @@ void setup() {
 
     // or override everything in one command;
     // It is also safe to use .begin() without .end() as it calls it internally
-    if (ESP32Can.begin(ESP32Can.convertSpeed(500), CAN_TX, CAN_RX, 10, 10)) {
+    if(ESP32Can.begin(ESP32Can.convertSpeed(500), CAN_TX, CAN_RX, 10, 10)) {
         Serial.println("CAN bus started!");
     } else {
         Serial.println("CAN bus failed!");
@@ -61,16 +61,16 @@ void loop() {
     static uint32_t lastStamp    = 0;
     uint32_t        currentStamp = millis();
 
-    if (currentStamp - lastStamp > 1000) { // sends OBD2 request every second
+    if(currentStamp - lastStamp > 1000) { // sends OBD2 request every second
         lastStamp = currentStamp;
         sendObdFrame(5); // For coolant temperature
     }
 
     // You can set custom timeout, default is 1000
-    if (ESP32Can.readFrame(rxFrame, 1000)) {
+    if(ESP32Can.readFrame(rxFrame, 1000)) {
         // Comment out if too many frames
         Serial.printf("Received frame: %03X  \r\n", rxFrame.identifier);
-        if (rxFrame.identifier == 0x7E8) {                                   // Standard OBD2 frame responce ID
+        if(rxFrame.identifier == 0x7E8) {                                    // Standard OBD2 frame responce ID
             Serial.printf("Collant temp: %3d°C \r\n", rxFrame.data[3] - 40); // Convert to °C
         }
     }
